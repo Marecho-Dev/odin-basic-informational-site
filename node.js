95b0b4d46fe3b2c67f06e96af8f509827a08fe97
Mareks1993 <marekstaszkiewicz@live.com>
2023-08-22 12:00:39 -0400

    node.js path switch created as well as readFile functionality

--- node.js
+++ node.js
@@ -1,6 +1,7 @@
 //HTTP MODULE - functionality for creating HTTP servers and clients
 const http = require('http');
-
+const path = require('path');
+const fs = require('fs');
 //hostname and port define where the server will run. 3000 common port
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -9,9 +10,39 @@ const port = 3000;
 //http.createServer is a callback that will be executed every time the server recieves a new request. req and res
 //represent the incoming message(request) and the server response
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
+    //path.join concats all the strings together..__dirname pulls current directory that the node.js file is in
+    //switch do have cases for different req.url that will append them to the end of the directory
+    let filePath;
+    switch (req.url) {
+        case '/':
+          filePath = path.join(__dirname, 'index.html');
+          break;
+        case '/contact-me':
+          filePath = path.join(__dirname, 'contact-me.html');
+          break;
+        case '/about':
+          filePath = path.join(__dirname, 'about.html');
+          break;
+        default:
+          filePath = path.join(__dirname, '404.html');
+          break;
+      }
+      fs.readFile(filePath, 'utf8', (err, content) => {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                // Ideally, this case should not be hit because we are already handling
+                // the default case in our switch statement, but it's a safeguard.
+                res.writeHead(404, {'Content-Type': 'text/plain'});
+                res.end('Not Found');
+            } else {
+                res.writeHead(500, {'Content-Type': 'text/plain'});
+                res.end('Server Error');
+            }
+        } else {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(content, 'utf8');
+        }
+    });
   });
 
 
